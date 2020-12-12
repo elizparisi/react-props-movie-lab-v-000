c874910a6422d2099078fb3c6357e6322c4055f5
Liz P <parisielizabeth@gmail.com>
2020-12-12 06:38:07 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -13,7 +13,10 @@ export default class CardBack extends Component {
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
     if (this.props.IMDBRating === null){
-      
+      return 'No Rating Found'
+    }
+    else {
+      return 
     }
   }
 
