9c85c6ac01afa64943fa2ecd9c8d1159fd8c6691
Liz P <parisielizabeth@gmail.com>
2020-12-12 06:14:09 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -44,6 +44,6 @@ export default class MovieCard extends Component {
 MovieCard.defaultProps = {
   title: 'Unknown',
   IMDBRating: null,
-  genres: ['No Genre(s) found'],
+  genres: ['No Genre(s) Found'],
   poster: 'default'
 }
